c207170c669e5a3d6735d8b7643aaf3d1ade3bbe
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-23 17:36:48 +0200

    Update structure

--- public/main.js
+++ public/main.js
@@ -1,72 +1,76 @@
 $(document).ready(function() {
-//Take user to edit poll page. in order to edit a poll.
-  $('.edit_btn').click(function singlePoll() {
-    var id = location.pathname.split('/')[3]
-    $.ajax({
-      url: '/polls/newpoll/',
-      type: 'GET',
-      success: function(result) {
-        document.location.href = '/polls/singlepoll/' + id;
-      },
-      error: function() {
-        alert('Cannot find poll. Something went wrong');
-      }
-    });
-  })
-// When the additional option button is clicked, add a new option text field to the list.
-  $('.choice_btn').click(function() {
-    var newOptionNum = $('.poll-option').length + 1;
-    var newName = 'option' + newOptionNum;
-    $("#options").append('<input class="poll-option" type="text" placeholder="Additional Option" name="' + newName + '" />' )
+	//Take user to edit poll page. in order to edit a poll.
+	$('.edit_btn').click(function singlePoll() {
+		var id = location.pathname.split('/')[3];
+		$.ajax({
+			url: '/polls/newpoll/',
+			type: 'GET',
+			success: function(result) {
+				document.location.href = '/polls/singlepoll/' + id;
+			},
+			error: function() {
+				alert('Cannot find poll. Something went wrong');
+			}
+		});
+	});
+	// When the additional option button is clicked, add a new option text field to the list.
+	$('.choice_btn').click(function() {
+		var newOptionNum = $('.poll-option').length + 1;
+		var newName = 'option' + newOptionNum;
+		$('#options').append(
+			'<input class="poll-option" type="text" placeholder="Additional Option" name="' +
+				newName +
+				'" />'
+		);
 
-    $('#options').off('input', 'input.poll-option', handleInputChange);
-    $('#options').on('input', 'input.poll-option', handleInputChange);
-  });
+		$('#options').off('input', 'input.poll-option', handleInputChange);
+		$('#options').on('input', 'input.poll-option', handleInputChange);
+	});
 
-//Make the submit button on the new poll creation page active only after the fields have been filled.
-  function handleInputChange(e) {
-    if (e.target.value) {
-      $('#submit_btn').prop('disabled', false);
-    } else {
-      $('#submit_btn').prop('disabled', true);
-    }
-  }
+	//Make the submit button on the new poll creation page active only after the fields have been filled.
+	function handleInputChange(e) {
+		if (e.target.value) {
+			$('#submit_btn').prop('disabled', false);
+		} else {
+			$('#submit_btn').prop('disabled', true);
+		}
+	}
 
-  $('#options').on('input', 'input.poll-option', handleInputChange);
+	$('#options').on('input', 'input.poll-option', handleInputChange);
 
-  $('#submit_btn').prop('disabled', true);
+	$('#submit_btn').prop('disabled', true);
 
-  $('#vote_btn').prop('disabled', true);
+	$('#vote_btn').prop('disabled', true);
 
-  $('input[type="radio"]').on('change', function() {
-    $('#vote_btn').prop('disabled', false);
-  });
-})
+	$('input[type="radio"]').on('change', function() {
+		$('#vote_btn').prop('disabled', false);
+	});
+});
 
 // Give users to delete a poll.
 $('.delete_btn').click(function() {
-  var id = this.id;
-  $.ajax({
-    url: '/polls/deletepoll/' + id,
-    type: 'DELETE',
-    success: function(result) {
-      alert("Item Deleted");
-      window.location.reload();
-    },
-    error: function() {
-      alert("The item cannot be deleted at this time.");
-    }
-  });
-})
+	var id = this.id;
+	$.ajax({
+		url: '/polls/deletepoll/' + id,
+		type: 'DELETE',
+		success: function(result) {
+			alert('Item Deleted');
+			window.location.reload();
+		},
+		error: function() {
+			alert('The item cannot be deleted at this time.');
+		}
+	});
+});
 
 // Copy URL to clipboard in-order for users to share a specific poll.
 $('.share_btn').click(function() {
-  var urlText = document.createElement("textarea");
-  urlText.style.background = 'transparent';
-  urlText.value = location.href;
-  document.body.appendChild(urlText);
-  urlText.select();
-  document.execCommand('copy');
-  document.body.removeChild(urlText);
-  alert("The poll link was successfully copied!");
-})
+	var urlText = document.createElement('textarea');
+	urlText.style.background = 'transparent';
+	urlText.value = location.href;
+	document.body.appendChild(urlText);
+	urlText.select();
+	document.execCommand('copy');
+	document.body.removeChild(urlText);
+	alert('The poll link was successfully copied!');
+});
--- server.js
+++ server.js
@@ -8,31 +8,40 @@ const session = require('express-session');
 
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/voting', {
-  useMongoClient: true
-});
+mongoose.connect(
+	process.env.MONGODB_URI || 'mongodb://localhost/voting',
+	{
+		useMongoClient: true
+	}
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json('*/*'));
 app.use(logger('combined'));
-app.use(session({secret: 'voteonitagainagain', saveUninitialized: true, resave: true}));
+app.use(
+	session({
+		secret: 'voteonitagainagain',
+		saveUninitialized: true,
+		resave: true
+	})
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('*', function(req, res, next) {
-  res.locals.user = req.user || null;
-  res.locals.error = null;
-  next();
+	res.locals.user = req.user || null;
+	res.locals.error = null;
+	next();
 });
 
-const routes =  require('./routes');
+const routes = require('./routes');
 app.use('/', routes);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log('Express server running on port', port)
+	console.log('Express server running on port', port);
 });
